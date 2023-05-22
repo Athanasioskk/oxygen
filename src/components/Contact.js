5039ab1a821b8c4648c20dd092d010bf071eaba5
Athanasios Koutsioukis <thanaseskouts@gmail.com>
2023-05-22 13:20:43 +0300

    main

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,23 @@
-
+import "../components/styles/Contact.css";
 
 function Contact() {
-    return;
+    return (
+        <div className="OverallContainer">
+            <div className="ContactContainer">
+                <div className="ContactFormContainer">
+                    <form type="submit">
+                        <h2>You may contact the owner directly via email.</h2>
+                        <label>From:</label>
+                        <input type="email" placeholder="example@email.com"></input>
+                        <label>To:</label>
+                        <input type="email" placeholder="example@email.com"></input>
+                        <input className="Email" type="text" placeholder="Write something..."></input>
+                    </form>
+                </div>
+                <div className="ContactInfo"></div>
+            </div>
+        </div>
+    )
 }
 
 export default Contact;
